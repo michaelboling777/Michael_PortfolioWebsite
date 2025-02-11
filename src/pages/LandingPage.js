1e293719dcc45b71bd49037adfad0c5608234f07
michaelboling777 <michaelboling30@gmail.com>
2025-02-10 18:13:14 -0600

    update

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -320,7 +320,7 @@ const LandingPage = () => {
         
         <div className="relative z-20 flex flex-col items-center justify-center h-full text-white text-center px-4">
           <h1 className="text-4xl md:text-5xl font-bold mb-4">Michael Boling</h1>
-          <p className="text-lg md:text-xl text-gray-300">Computer Science Graduate | Aspiring Software Developer</p>
+          <p className="text-lg md:text-xl text-gray-300">Computer Programmar Analyst (UPDATED!)</p>
         </div>
       </section>
 
